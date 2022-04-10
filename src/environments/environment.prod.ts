515ed3352d6911d847850d7a2ba5a4ce6f327182
unknown <gabeclementej@gmail.com>
2022-04-10 18:53:51 -0300

    fix environment url API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  urlApiGp: 'http://cursos.grandeporte.com.br:8080',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  urlApiGp: 'http://cursos.grandeporte.com.br:8080',
 };
 
 /*
